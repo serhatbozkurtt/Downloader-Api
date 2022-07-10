2d911ebc1766a04474f4c932f64b4babd9e5b6d5
serhatbozkurtt <65898491+serhatbozkurtt@github.com>
2022-07-11 02:41:17 +0300

    get_web created

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import get
 import get_mobil
+import get_web
+from flask import send_file
 
 app = Flask(__name__)
 
@@ -22,6 +24,20 @@ def down():
         res = get_mobil.link_js(req_data)
         return jsonify({'video_name':res})
     return jsonify({'todos':'TODOS'})
+
+@app.route("/web", methods=['GET', 'POST'])
+def web():
+    if request.method == 'POST':
+        link = request.form.get("link")
+        print("link " + link)
+        response = get_web.main(link)
+        #response = "20220710134318014559.mp3"
+        filename = response
+        #with open('/tmp/output.mp3', 'wb') as out:
+        #    out.write(response.audio_content)
+
+        return send_file("%s" %filename, as_attachment=True)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
---
+++ get_web.py
@@ -0,0 +1,78 @@
+from pytube import YouTube
+import os
+from datetime import datetime
+from time import gmtime, strftime
+import pyrebase
+import json
+
+
+def main(link):
+    #print("mobil linki -->> " + filename)
+
+    #link = "https://www.youtube.com/watch?v=Y2tw4fNEfbI"
+    print("link girildi")
+
+    try:
+        yt = YouTube(link)
+    except:
+        print("Invalid link")
+        exit()
+
+    mpt = yt.streams.filter(only_audio=True).first()
+
+    filename = upload(mpt)
+
+    return filename
+
+
+def upload(mpt):
+    config = {
+        "apiKey": "AIzaSyBkALe1CYOOfQyLNa7Q2odd0P6LZ91jxrA",
+        "authDomain": "ytmp3-f6bcc.firebaseapp.com",
+        "projectId": "ytmp3-f6bcc",
+        "storageBucket": "ytmp3-f6bcc.appspot.com",
+        "messagingSenderId": "5264877193",
+        "appId": "1:5264877193:web:a59238198e3e2498e94858",
+        "measurementId": "G-ZBX5927KVR",
+        "serviceAccount": "serviceAccount.json",
+        "databaseURL": "https://ytmp3-f6bcc-default-rtdb.firebaseio.com/"
+    }
+    firebase = pyrebase.initialize_app(config)
+    storage = firebase.storage()
+    # upload
+    # storage.child("s.mp3").put("/audio/MySong.mp3")
+    # storage.child("gs_up.jpg").put("/static/images/gs.jpg")
+
+    filename1 = 'get_v_flask2'
+    filename = datetime.now().strftime('%Y%m%d%H%M%S%f')
+
+    fn = filename + '.mp3'
+
+    output = mpt.download(filename = fn)
+
+    print("storage a geldi")
+
+    storage.child("%s" %fn).put("%s" %fn)
+
+    print("storage bitti")
+
+    return fn
+
+
+def download():
+    config = {
+        "apiKey": "AIzaSyBkALe1CYOOfQyLNa7Q2odd0P6LZ91jxrA",
+        "authDomain": "ytmp3-f6bcc.firebaseapp.com",
+        "projectId": "ytmp3-f6bcc",
+        "storageBucket": "ytmp3-f6bcc.appspot.com",
+        "messagingSenderId": "5264877193",
+        "appId": "1:5264877193:web:a59238198e3e2498e94858",
+        "measurementId": "G-ZBX5927KVR",
+        "serviceAccount": "serviceAccount.json",
+        "databaseURL": "https://ytmp3-f6bcc-default-rtdb.firebaseio.com/"
+    }
+    firebase = pyrebase.initialize_app(config)
+    storage = firebase.storage()
+
+    print("storage a geldi")
+    storage.download("20220710164110798595.mp3", "asdasd.mp3")
